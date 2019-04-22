55f92a12666562bda9cf4a330caf7df1f4c46961
rhinamaarin <47165669+rhinamaarin@users.noreply.github.com>
2019-04-21 22:06:02 -0600

    Update DatabaseX.java

--- ProyectoFinalPrograII/src/java/Inventory/prografinal/database/DatabaseX.java
+++ ProyectoFinalPrograII/src/java/Inventory/prografinal/database/DatabaseX.java
@@ -22,7 +22,7 @@ public class DatabaseX
                 + "&useLegacyDatetimeCode=false"
                 + "&serverTimezone=UTC";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "clayre";
+    private static final String DB_PASSWORD = "timel";
 
     
     //para hacer la conexion
